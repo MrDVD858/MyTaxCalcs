8a95e13dfcc3dbe90c53f3c41e51ebdfbc06bfdf
MrDVD858 <dvando@gmail.com>
2026-04-26 15:17:47 -0700

    Expand sitemap to include all state calculator pages

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const states = require("../data/states");
+
+const baseUrl = "https://mytaxcalcs.com";
+
+const staticUrls = [
+  "/",
+  "/income-tax-calculator",
+  "/about",
+  "/contact",
+  "/privacy-policy",
+  "/terms",
+  "/disclaimer"
+];
+
+const stateUrls = states.map((state) => `/${state.slug}-income-tax-calculator`);
+
+const allUrls = [...staticUrls, ...stateUrls];
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allUrls
+  .map((url) => {
+    return `  <url>
+    <loc>${baseUrl}${url}</loc>
+  </url>`;
+  })
+  .join("\n")}
+</urlset>
+`;
+
+const outputPath = path.join(__dirname, "..", "public", "sitemap.xml");
+
+fs.writeFileSync(outputPath, sitemap, "utf8");
+
+console.log(`Sitemap generated with ${allUrls.length} URLs.`);
+console.log(`Saved to: ${outputPath}`);
