2268fc6f3636503daafd03009da7e02582d56c63
MrDVD858 <67932230+MrDVD858@users.noreply.github.com>
2026-05-30 19:02:17 -0700

    remove files

--- diagnose.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('views/tax-refund-calculator.ejs', 'utf8');
-const i = c.indexOf('<ins class="adsbygoogle"');
-console.log(JSON.stringify(c.slice(i - 100, i + 400)));
--- diagnose2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('views/tax-refund-calculator.ejs', 'utf8');
-
-// Find the ad-zone div
-const start = content.indexOf('ad-zone-label');
-const chunk = content.slice(start - 50, start + 600);
-
-// Print as JSON so we see every whitespace character exactly
-console.log(JSON.stringify(chunk));
